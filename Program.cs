07a54da6da74edaea3caeb25aa6682b4439f505b
Sandeep Chourasia <schaurasiya1211@gmail.com>
2025-11-27 11:42:09 +0530

    [Sandeep Spy code done]

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using TopAskedQues.SpyNumberCheck;
+
+namespace TopAskedQues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat;
+
+            do
+            {
+                Console.WriteLine("------------ MENU ------------");
+                Console.WriteLine("Option 1 : Check the Given No is Spy Or Not");
+                Console.WriteLine("Option 2 : Check the Given No is Neon Or Not");
+                Console.WriteLine("Option 3 : Sum of Odd digit is Prime or Not");
+                Console.WriteLine("Option 4 : Check the Given No is Strong Or Not");
+                Console.WriteLine("Option 5 : Reverse The Given number");
+                Console.WriteLine("Option 6 : Check the Given No is Palindrome Or Not");
+                Console.WriteLine("Option 7 : Check the Given No contains Only Even digit Or Not");
+                Console.WriteLine("Option 8 : Check the Given No contains Any Odd digit Or Not");
+                Console.WriteLine("Option 9 : Check the Given No is ArmStrong Or Not");
+                Console.WriteLine("Option 10 :Print N Prime Number");
+                Console.WriteLine("Option 11 :Print Nth Prime Number");
+                Console.WriteLine("Option 12 :Sum of each digit untill getting Single digit");
+                Console.WriteLine("Option 13 : Check the Given No is Happy Number Or Not");
+                Console.WriteLine("Option 14 : Print Nearest Prime");
+                Console.WriteLine("------------------------------");
+
+                Console.Write("Please choose an option : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                if (option == 1)
+                {
+                    printSpy();
+                }
+                else if (option == 2)
+                {
+                    
+                }
+                else if (option == 3)
+                {
+               
+
+                }
+                else if (option == 4)
+                {
+                    
+                }
+                else if (option == 5)
+                {
+                   
+                }
+                else if (option == 6)
+                {
+                    
+                }
+                else if (option == 7)
+                {
+                    
+                }
+                else if (option == 8)
+                {
+                   
+                }
+                else if (option == 9)
+                {
+                  
+                }
+                else if (option == 10)
+                {
+                    
+                }
+                else if (option == 11)
+                {
+                  
+                }
+                else if (option == 12)
+                {
+                   
+
+                }
+                else if (option == 13)
+                {
+                  
+                }
+                else if (option == 14)
+                {
+                  
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option! Try again.");
+                }
+
+                Console.WriteLine();
+                Console.Write("Do you want to continue? (y/n): ");
+                string ans = Console.ReadLine();
+
+                repeat = ans.ToLower() == "y";
+
+                Console.WriteLine();
+
+            } while (repeat);
+
+        }
+
+        public static void printSpy()
+        {
+            Console.WriteLine("Enter The Number to Check Spy : ");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            SpyNumber sN = new SpyNumber();
+            bool res=sN.isSpy(num);
+
+            if (res)
+                Console.WriteLine($"The {num} is Spy Number");
+            else
+                Console.WriteLine($"The {num} is Not Spy Number");
+        }
+    }
+}
---
+++ SpyNumberCheck/SpyNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopAskedQues.SpyNumberCheck
+{
+    internal class SpyNumber
+    {
+        // Spy No-- Sum of Each Digit Should be Equal to product of Each Digit
+
+        public SpyNumber() { }
+
+        public Boolean isSpy(int num)
+        {
+            int sum = 0; int prod = 1;
+
+            while (num>0)
+            {
+                int temp = num % 10;
+
+                sum += temp;
+                prod *= temp;
+
+                num /= 10;
+            }
+            return sum == prod;
+        }
+    }
+}
